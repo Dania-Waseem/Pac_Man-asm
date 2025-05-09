70580886f38b6c3b50c1a792f2f52d551e886a16
Dania-Waseem <i232622@isb.nu.edu.pk>
2025-05-09 22:20:57 +0500

    added pacman title

---
+++ ASSEMBLY2/CODE.asm
@@ -0,0 +1,26 @@
+; 23i-2622
+; DS A
+; COAL SEMESTER PROJECT 
+
+include irvine32.inc
+
+.data
+
+	pmtitle1 db "%%%%%%%%%%%%    @@@@@@@@@@  %%%%%%%%%%%%	 @@@@@@  @@@@@@  @@@@@@@@@@  %%%        %%     ",0  
+	pmtitle2 db "%%		   %%    @	      @  %%			     @    @  @    @  @	      @  %% %       %%     ",0
+	pmtitle3 db "%%        %%    @	      @  %%			     @    @  @    @  @	      @  %%  %      %%	   ",0
+	pmtitle4 db "%%        %%	 @	      @  %%			     @    @  @    @  @	      @  %%   %     %%     ",0
+	pmtitle5 db "%%%%%%%%%%%%	 @@@@@@@@@@  %%		 ######  @    @@@@    @  @@@@@@@@@@  %%    %    %%	   ",0
+	pmtitle5 db "%%				 @	      @  %%			     @            @  @	      @  %%     %   %%     ",0
+	pmtitle6 db "%%				 @	      @  %%			     @            @  @	      @  %%      %  %%     ",0
+	pmtitle7 db "%%		         @	      @  %%			     @            @  @	      @  %%       % %%     ",0
+	pmtitle8 db "%%		         @	      @  %%%%%%%%%%%%	 @            @  @        @  %%        %%%     ",0
+
+.code
+main proc
+
+main endp
+
+
+
+end main
