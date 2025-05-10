5a408929da33c9986d6cd6f8c692d6632c65f3df
Dania-Waseem <i232622@isb.nu.edu.pk>
2025-05-10 22:24:51 +0500

    menu screen

--- ASSEMBLY2/CODE.asm
+++ ASSEMBLY2/CODE.asm
@@ -5,12 +5,13 @@
 include irvine32.inc
 
 .data
-
+    
+    fileName  db "Scores.txt" 
 	inputName db "Enter your name ... ",0
-    username db 50 dup(0)
+    username  db 50 dup(0)
 
     pmtitle1 db "#################################################################################",0
-    pmtitle2 db "#                                                                               # ",0
+    pmtitle2 db "#                                                                               #",0
     pmtitle3 db "#      %%%%%%%%  %%%%%%%  %%%%%%%%     %%%%%   %%%%%  %%%%%%%%   %%      %      #",0
 	pmtitle4 db "#      %%     %  %     %  %            %   %   %   %  %      %   % %     %      #",0
 	pmtitle5 db "#      %%     %  %     %  %            %   %   %   %  %      %   %  %    %      #",0
@@ -20,6 +21,52 @@ include irvine32.inc
 	pmtitle9 db "#      %%        %     %  %%%%%%%%     %           %  %      %   %      %%      #",0
 	pmtitle10 db"#                                                                               #",0
 	pmtitle11 db"#################################################################################",0
+
+    menutitle0 db " ___     ___ _________  ___     ___ ___   ___ ",0
+    menutitle1 db " |  \   /  | |  ______| |  \    | | | |   | | ",0
+    menutitle2 db " | |\\_//| | | |______  | |\\   | | | |   | |",0
+    menutitle3 db " | | \_/ | | | _______| | | \\  | | | |   | | ",0
+    menutitle4 db " | |     | | | |______  | |  \\ | | | |___| |",0
+    menutitle5 db " |_|     |_| |________| |_|   \\|_| |_______| ",0
+
+    menuopt0 db "---------------------------------",0
+    menuopt1 db "|  1. Start Game ( 3 levels)    |",0
+    menuopt2 db "|  2. Display Game Instructions |",0
+    menuopt3 db "|  3. View Highscores /scores   |",0
+    menuopt4 db "|  4. Exit Game                 |",0
+    menuopt5 db "|_______________________________|",0
+
+    menuoptchoose db "Enter your choice:  ",0
+
+    level1opt0 db "*     ***** *       * *****  *          **    ",0
+    level1opt1 db "*     *      *     *  *      *          **    ",0
+    level1opt2 db "*     *****   *   *   *****  *          **    ",0
+    level1opt3 db "*     *        * *    *      *          **    ",0
+    level1opt4 db "***** *****     *     *****  *****      **    ",0
+
+    level2opt0 db "*     ***** *       * *****  *         *****  ",0
+    level2opt1 db "*     *      *     *  *      *             *  ",0
+    level2opt2 db "*     *****   *   *   *****  *         *****  ",0
+    level2opt3 db "*     *        * *    *      *         *      ",0
+    level2opt4 db "***** *****     *     *****  *****     *****  ",0
+
+    level3opt0 db "*     ***** *       * *****  *         *****  ",0
+    level3opt1 db "*     *      *     *  *      *             *  ",0
+    level3opt2 db "*     *****   *   *   *****  *         *****  ",0
+    level3opt3 db "*     *        * *    *      *             *  ",0
+    level3opt4 db "***** *****     *     *****  *****     *****  ",0
+
+    leveloptchoose db " Enter you choice (1: Level 1, 2: Level 2 , 3: Level 3)",0
+
+
+
+
+
+
+
+
+
+
 
     redTxt = red+(black*16);
     yellowTxt =yellow+(black*16);
@@ -38,19 +85,19 @@ main proc
 
     
     mov dh,5
-    mov dl,10
+    mov dl,12
     call Gotoxy
     mov edx, offset pmtitle1
     call WriteString
 
     mov dh, 6
-    mov dl, 10
+    mov dl, 12
     call Gotoxy
     mov edx, offset pmtitle2
     call WriteString
 
     mov dh, 7
-    mov dl, 10
+    mov dl, 12
     call Gotoxy
     mov edx, offset pmtitle3
     call WriteString
@@ -61,26 +108,26 @@ main proc
     call SetTextColor
 
     mov dh, 8
-    mov dl, 10
+    mov dl, 12
     call Gotoxy
     mov edx, offset pmtitle4
     call WriteString
 
     mov dh, 9
-    mov dl, 10
+    mov dl, 12
     call Gotoxy
     mov edx, offset pmtitle5
     call WriteString
 
     mov dh, 10
-    mov dl, 10
+    mov dl, 12
     call Gotoxy
     mov edx, offset pmtitle6
     call WriteString
 
 
     mov dh, 11
-    mov dl, 10
+    mov dl, 12
     call Gotoxy
     mov edx, offset pmtitle7
     call WriteString
@@ -91,26 +138,26 @@ main proc
     call SetTextColor
 
     mov dh, 12
-    mov dl, 10
+    mov dl, 12
     call Gotoxy
     mov edx, offset pmtitle8
     call WriteString
 
 
     mov dh,13
-    mov dl,10
+    mov dl,12
     call Gotoxy
     mov edx, offset pmtitle9
     call WriteString
 
     mov dh, 14
-    mov dl, 10
+    mov dl, 12
     call Gotoxy
     mov edx, offset pmtitle10
     call WriteString
 
     mov dh, 15
-    mov dl, 10
+    mov dl, 12
     call Gotoxy
     mov edx, offset pmtitle11
     call WriteString
@@ -120,7 +167,7 @@ main proc
     call SetTextColor
 
     mov dh, 19
-    mov dl, 10
+    mov dl, 12
     call Gotoxy
     mov edx, offset inputName
     call WriteString
